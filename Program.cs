a930002ee1dcbc8a4ee895ed9f4c2caca57d467d
MyllyAnni <annimyllyniemi7@msn.com>
2025-08-08 09:17:50 +0300

    Lisätty kirjan luominen ja lisääminen listaan.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Kirjat;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Kirja> listaKirjoista = new List<Kirja>();
+
+        Console.WriteLine("Kirjan nimi: ");
+        string nimi = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Kirjoittaja: ");
+        string kirjoittaja = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Julkaisuvuosi: ");
+        int vuosi = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Genre: ");
+        string genre = Console.ReadLine() ?? "";
+
+        Kirja uusiKirja = new Kirja(nimi, kirjoittaja, vuosi, genre);
+
+        Kirja.LisääKirja(listaKirjoista, uusiKirja);      
+        Console.WriteLine("Kirja lisätty!");
+    }
+}
